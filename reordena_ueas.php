5fb37ebce921c2a11f993a74a9afc2267a08d78b
Giovanni Olmos <xidotwo@gmail.com>
2021-09-17 12:22:27 -0500

    Temas y UEAS (Orden)
    En camino para  ordenar temas y UEAS

---
+++ reordena_ueas.php
@@ -0,0 +1,13 @@
+<?php
+   require_once('db_access.php');
+   require_once('db_auth.php');
+   $conexion = new db_access(HOST_DB, USER_DB, PASSWORD_DB, DATABASE_DB);  
+    
+   if (isset($_POST['arr'])) { 
+      $arr = json_decode($_POST['arr'],true);
+      for ($i = 0; $i < count($arr); ++$i) {  
+         $conexion->query("UPDATE ueas SET orden = ? WHERE id = ?", $i , $arr[$i]);
+      }    
+      die(true);     
+   }
+?>
