55283acd37c738633fdf15bf510da6e846aff4a6
Rodrigo Castro <rcc.dark@gmail.com>
2021-10-03 13:24:59 -0500

    permitiendo mover problemas entre uea fácilmente
    va

--- base/drop.js
+++ base/drop.js
@@ -7,28 +7,30 @@ function habilita_drag(objeto, tipo) {
    };
 }
 
-function habilita_drop_hermano(objeto, tipo, hermano, notifica_drop) {     
+function habilita_drop(objeto, tipo, funcion) {     
    objeto.ondragover = (evento) => evento.preventDefault( );
    objeto.addEventListener("drop", function(evento) {
       let adoptar = document.getElementById(evento.dataTransfer.getData("text/plain"));
       if (adoptar.getAttribute("data-droptype") == tipo) {
-         hermano.parentNode.insertBefore(adoptar, hermano);
-         if (notifica_drop != undefined) {
-            notifica_drop(hermano, adoptar);
-         }
+         funcion(adoptar);
+      }
+   });
+}
+
+function habilita_drop_hermano(objeto, tipo, hermano, notifica_drop) {
+   habilita_drop(objeto, tipo, function(adoptar) {
+      hermano.parentNode.insertBefore(adoptar, hermano);
+      if (notifica_drop != undefined) {
+         notifica_drop(hermano, adoptar);
       }
    });
 }
 
 function habilita_drop_hijo(objeto, tipo, padre, notifica_drop) {
-   objeto.ondragover = (evento) => evento.preventDefault( );
-   objeto.addEventListener("drop", function(evento) {
-      let adoptar = document.getElementById(evento.dataTransfer.getData("text/plain"));
-      if (adoptar.getAttribute("data-droptype") == tipo) {
-         padre.appendChild(adoptar);
-         if (notifica_drop != undefined) {
-            notifica_drop(padre, adoptar);
-         }
+   habilita_drop(objeto, tipo, function(adoptar) {
+      padre.appendChild(adoptar);
+      if (notifica_drop != undefined) {
+         notifica_drop(padre, adoptar);
       }
    });
 }
