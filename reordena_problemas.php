59d62ce2890b822853b22be9c520fad684c26be4
Giovanni Olmos <xidotwo@gmail.com>
2021-10-05 09:46:20 -0500

    Mover problemas en problemas.html
    Se mueven los problemas entre los diferentes temas de las UEAS y esta se ve reflejado en la base de datos

--- reordena_problemas.php
+++ reordena_problemas.php
@@ -3,11 +3,12 @@
    require_once('base/db_auth.php');
    $conexion = new db_access(HOST_DB, USER_DB, PASSWORD_DB, DATABASE_DB);  
     
-   if (isset($_POST['arr'])) { 
+   if (isset($_POST['arr']) && isset($_POST['id_Tema'])) { 
       $arr = json_decode($_POST['arr'], true);
+      $id_Tema = json_decode($_POST['id_Tema'], true);;
       for ($i = 0; $i < count($arr); ++$i) {  
-         $conexion->query("UPDATE problemas SET orden = ? WHERE id = ?", $i , $arr[$i]);
-      }    
+         $conexion->query("UPDATE problemas SET orden = ?,tema = ? WHERE id = ?", $i, $id_Tema , $arr[$i]);
+      }     
       die(true);     
    }
 ?>
